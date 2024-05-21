f3c1a7ca38231234e1db5df56f65d16cc211ca15
hectormartinet <hectormartinet@yahoo.fr>
2024-05-21 15:22:13 +0200

    refactoring suffix tree

--- network_generator/multi_order.py
+++ network_generator/multi_order.py
@@ -11,9 +11,8 @@ import tqdm
 class SuffixTree:
     def __init__(self, input_file=None):
         self.weight = 0
-        self.entropy = None
         self.children = {}
-        self.next_weight = {}
+        self.transition_weight = {}
         if input_file is not None:
             with open(input_file, 'r') as openfile:
                 data = json.load(openfile)
@@ -21,7 +20,7 @@ class SuffixTree:
 
     def load(self, data):
         self.weight = data["weight"]
-        self.next_weight = data["next_weight"]
+        self.transition_weight = data["transition_weight"]
         for node, child in data["children"].items():
             tree = SuffixTree()
             tree.load(child)
@@ -30,7 +29,7 @@ class SuffixTree:
     def to_dict(self):
         data = {}
         data["weight"] = self.weight
-        data["next_weight"] = self.next_weight
+        data["transition_weight"] = self.transition_weight
         data["children"] = {}
         for node, child in self.children.items():
             data["children"][node] = child.to_dict()
@@ -41,7 +40,13 @@ class SuffixTree:
             json.dump(self.to_dict(), outfile)
     
     def next_proba(self, node):
-        return self.next_weight[node]/self.weight if node in self.next_weight else 0
+        return self.transition_weight[node]/self.weight if node in self.transition_weight else 0
+
+    def entropy(self):
+        return -sum([weight*np.log2(weight) for weight in self.transition_weight.values()])/self.weight + np.log2(self.weight)
+
+    def relative_entropy(self, child):
+        return sum([child.next_proba(node)*np.log2(child.next_proba(node)/self.next_proba(node)) for node in child.transition_weight.keys()])
     
     def pattern_depth(self, lst_nodes, copy_node_lst=True):
         if copy_node_lst:
@@ -61,23 +66,23 @@ class SuffixTree:
                 return self.children[last_node].proba(lst_nodes, node, copy_node_lst=False)
         return self.next_proba(node)
     
-    def get_node_info(self, lst_nodes, copy_node_lst=True):
+    def get_tree(self, lst_nodes, copy_node_lst=True):
         if copy_node_lst:
             lst_nodes = [node for node in lst_nodes]
         if lst_nodes:
             node = lst_nodes.pop()
             if node in self.children:
-                return self.children[node].get_tree(lst_nodes, node, copy_node_lst=False)
-        return self.weight, self.next_weight
+                return self.children[node].get_tree(lst_nodes, copy_node_lst=False)
+        return self
     
     def add_all_prefix_children(self, lst_nodes, next_node, copy_node_lst=True):
         self.weight += 1
         if copy_node_lst:
             lst_nodes = [node for node in lst_nodes]
-        if next_node in self.next_weight:
-            self.next_weight[next_node] += 1
+        if next_node in self.transition_weight:
+            self.transition_weight[next_node] += 1
         else :
-            self.next_weight[next_node] = 1
+            self.transition_weight[next_node] = 1
         if lst_nodes:
             node = lst_nodes.pop()
             if node not in self.children:
@@ -100,15 +105,15 @@ class SuffixTree:
             child.similarity_pruning(K)
         for node, child in self.children.items():
             if child.children: continue
-            non_similarity = sum([child.next_proba(node)*np.log(child.next_proba(node)/self.next_proba(node)) for node in child.next_weight.keys()])*child.weight
-            if non_similarity < K:
+            info_gain = self.relative_entropy(child)*child.weight
+            if info_gain < K:
                 nodes_to_remove.append(node)
         for node in nodes_to_remove:
             self.children.pop(node)
 
     def stringify(self, depth=0):
         output = "root:" + str(self.weight) + "\n" if depth==0 else ""
-        for node, weight in sorted(self.next_weight.items(), key=lambda x:-x[1]):
+        for node, weight in sorted(self.transition_weight.items(), key=lambda x:-x[1]):
             output += "|  "*depth + "[" + str(round(weight/self.weight,3)) + ":" + node + "\n" 
         for node,child in sorted(self.children.items(), key=lambda x:-x[1].weight):
             output += "|  "*depth + "├──" + str(child.weight) + " " + node + "\n"
@@ -117,11 +122,6 @@ class SuffixTree:
 
     def __str__(self):
         return self.stringify()
-
-    def compute_entropy(self):
-        self.entropy = -sum([weight*np.log(weight) for weight in self.next_weight.values()])/self.weight + np.log(self.weight)
-        for child in self.children.values():
-            child.compute_entropy()
     
     def nb_nodes(self):
         return 1 + sum([child.nb_nodes() for child in self.children.values()])
@@ -139,7 +139,7 @@ class SuffixTree:
             node = lst_nodes.pop()
             if node in self.children:
                 return self.children[node].random_next_node(lst_nodes, copy_node_lst=False)
-        return random.choices(list(self.next_weight.keys()), [elt**alpha for elt in self.next_weight.values()])[0]
+        return random.choices(list(self.transition_weight.keys()), [elt**alpha for elt in self.transition_weight.values()])[0]
     
     def generate_sequence(self, size, alpha=1.):
         # Can be optimized to copy only the last "max_order" elements of the list 
@@ -164,16 +164,17 @@ def apply_diatonic_interval(pitch, interval, sharp_number=0):
 
 if __name__ == "__main__":
 
-    t=3
+    t=5
     K=1
-    L=10
+    L=1000
 
     # directory = "datasets\\mozart_sonatas\\"
-    directory = "midis\\invent_bach\\"
+    # directory = "midis\\invent_bach\\"
     # midi_files = [directory + f for f in os.listdir(directory)]
-    midi_files = ["midis\\invent_bach\\invent14.mid"]
-    output_file = "suffix_tree.json"
-    net = MultiLayerNetwork(use_gui=False, enharmony=False, pitch=False, structure="monolayer", diatonic_interval=False, duration=True, midi_files=midi_files)
+    # midi_files = [str(elt) for elt in ms.corpus.getComposer('bach', fileExtensions=('xml',))]
+    midi_files = ["midis\\invent_bach\\invent1.mid"]
+    output_file = "trees\\bach_invent1_interval.json"
+    net = MultiLayerNetwork(use_gui=False, enharmony=False, pitch=False, structure="monolayer", diatonic_interval=True, duration=False, midi_files=midi_files)
     tree = SuffixTree()
     loading_bar = tqdm.tqdm(total=len(midi_files))
     for midi_file in midi_files:
@@ -184,21 +185,13 @@ if __name__ == "__main__":
         loading_bar.update(1)
     
     tree.weight_pruning(t=t)
-    tree.similarity_pruning(K=K)
+    # tree.similarity_pruning(K=K)
     # print(tree)
 
     # Test saving and loading
-    # tree.save(output_file)
+    tree.save(output_file)
     # loaded_tree = SuffixTree(output_file)
     # print(loaded_tree.nb_nodes())
     # print(loaded_tree.max_depth())
     # print(loaded_tree.weight)
     # print(loaded_tree)
-
-    node_lst = net._get_nodes_list(1)
-    acc = 0
-    for i in range(len(node_lst)):
-        proba = tree.proba(node_lst[max(0,L-i):i],node_lst[i],False)
-        acc += np.log(proba)
-        # print(node_lst[i],":",round(proba,3))
-    print(acc/len(node_lst))
