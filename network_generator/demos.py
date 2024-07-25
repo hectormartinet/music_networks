d3b07960f87fd9b9b57ea61bb7c6baafed4cbc25
hectormartinet <hectormartinet@yahoo.fr>
2024-07-25 19:17:00 +0200

    update demos

--- network_generator/demos.py
+++ network_generator/demos.py
@@ -6,7 +6,7 @@ class DemoMultilayer:
         self.outfolder = outfolder
 
     def run(self, **kwargs):
-        net = MultiLayerNetwork(use_gui=False, midi_files = [self.midi_file], outfolder=self.outfolder, **kwargs)
+        net = MultiLayerNetwork(use_gui=False, music_files = [self.midi_file], outfolder=self.outfolder, **kwargs)
         net.create_net(separate_graphs=True, output_txt=True, parallel=False)
         net.export_nets()
 
@@ -24,6 +24,10 @@ if __name__ == "__main__":
     # demo.run(name="offset_2", pitch=False, offset=True, beat_duration=2.)
     # demo.run(name="chromatic_interval", pitch=False, order=2, chromatic_interval=True)
     # demo.run(name="diatonic_interval", pitch=False, order=2, diatonic_interval=True, enharmony=False)
+    # demo.run(name="chords", pitch=True, structure="chordify")
+    demo.run(name="chordify_by_beat", pitch=True, chordify_per_beat=True)
+
+    # demo.run(name="pitch_duration", pitch=True, octave=True, duration=True, rest=True)
 
     # demo.run(name="duration_n2", pitch=False, duration=True, order=2)
     # demo.run(name="duration_n3", pitch=False, duration=True, order=3)
@@ -35,4 +39,4 @@ if __name__ == "__main__":
     # demo.run(name="ultimate", pitch=False, duration=True, group_by_beat=True, beat_duration=1, rest=True, diatonic_interval=True, enharmony=False)
 
     # demo.run(name="offset_duration", pitch=False, duration=True, offset=True, rest=True, beat_duration=2)
-    demo.run(name="offset_duration_interval", pitch=False, duration=True, offset=True, rest=True, enharmony=False, diatonic_interval=True, order=2)
+    # demo.run(name="offset_duration_interval", pitch=False, duration=True, offset=True, rest=True, enharmony=False, diatonic_interval=True, order=2)
